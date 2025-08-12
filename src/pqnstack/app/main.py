a5aef6fc67baac5b812bb07bd24c9075915e3548
Marcos Frenkel <38033108+marcosfrenkel@users.noreply.github.com>
2025-08-12 16:17:37 -0500

    Allowing cors (#102)

--- src/pqnstack/app/main.py
+++ src/pqnstack/app/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from pqnstack.app.api.main import api_router
 
@@ -9,6 +10,15 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title="Public Quantum Network",
+)
+
+# Add CORS middleware to allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 app.include_router(api_router)
